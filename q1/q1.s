b7be2062bfae808d90baa4e3692359c87d27db55
Neharika R <neharikarajesh05@gmail.com>
2026-04-14 23:42:34 +0530

    q1.s updated

--- q1/q1.s
+++ q1/q1.s
@@ -2,18 +2,17 @@
 
 .globl make_node
 make_node:
-    # a0: int val
     addi sp, sp, -16
     sd ra, 8(sp)
-    sw a0, 0(sp)     # save the value temporarily
+    sw a0, 0(sp)     
 
-    addi a0, zero, 24        # 24 bytes for struct Node
+    addi a0, zero, 24        
     call malloc      
     
     lw t0, 0(sp)     
-    sw t0, 0(a0)     # node->val = val
-    sd zero, 8(a0)   # node->left = NULL
-    sd zero, 16(a0)  # node->right = NULL
+    sw t0, 0(a0)     
+    sd zero, 8(a0)   
+    sd zero, 16(a0)  
 
     ld ra, 8(sp)
     addi sp, sp, 16
@@ -22,29 +21,25 @@ make_node:
 
 .globl insert
 insert:
-    # a0 = root, a1 = val
-    # simple recursive insertion
     bne a0, zero, insert_step_2
     
-    # if root == NULL, return a new node
     add a0, a1, zero
     j make_node
 
 insert_step_2:
     addi sp, sp, -32
     sd ra, 24(sp)
-    sd s0, 16(sp)      # s0 will hold root
-    sd s1, 8(sp)       # s1 will hold val
+    sd s0, 16(sp)      
+    sd s1, 8(sp)       
 
     add s0, a0, zero          
     add s1, a1, zero          
 
-    lw t0, 0(s0)       # root->val
-    beq s1, t0, end_insert  # ignore duplicate
+    lw t0, 0(s0)       
+    beq s1, t0, end_insert  
     blt s1, t0, insert_left
 
 insert_right:
-    # root->right = insert(root->right, val)
     ld a0, 16(s0)
     add a1, s1, zero
     call insert
@@ -52,14 +47,14 @@ insert_right:
     j end_insert
 
 insert_left:
-    # root->left = insert(root->left, val)
+    
     ld a0, 8(s0)
     add a1, s1, zero
     call insert
     sd a0, 8(s0)
 
 end_insert:
-    add a0, s0, zero          # return root
+    add a0, s0, zero          
     ld ra, 24(sp)
     ld s0, 16(sp)
     ld s1, 8(sp)
@@ -69,20 +64,20 @@ end_insert:
 
 .globl get
 get:
-    # a0 = root, a1 = val
+    
     beq a0, zero, get_done
     
     lw t0, 0(a0)
-    beq a1, t0, get_done    # return current node if found
+    beq a1, t0, get_done    
 
     blt a1, t0, get_left
     
-    # go right
+    
     ld a0, 16(a0)
     j get
     
 get_left:
-    # go left
+    
     ld a0, 8(a0)
     j get
 
@@ -92,28 +87,28 @@ get_done:
 
 .globl getAtMost
 getAtMost:
-    # a0 = val, a1 = root
-    addi a2, zero, -1          # keep track of the largest valid element
+    
+    addi a2, zero, -1          
 
 loop_start:
     beq a1, zero, end_of_func
-    lw t0, 0(a1)       # root->val
+    lw t0, 0(a1)       
     
     beq t0, a0, found_the_value 
     bgt t0, a0, gam_left  
     
-    add a2, t0, zero          # save the best element we've seen so far!
-    ld a1, 16(a1)      # keep searching right for potentially larger valid ones
+    add a2, t0, zero          
+    ld a1, 16(a1)      
     j loop_start
 
 gam_left:
-    ld a1, 8(a1)       # go left
+    ld a1, 8(a1)       
     j loop_start
 
 found_the_value:
-    add a0, t0, zero          # return exact match
+    add a0, t0, zero          
     ret
 
 end_of_func:
-    add a0, a2, zero          # return the largest valid one recorded
+    add a0, a2, zero          
     ret
